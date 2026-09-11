d5f3fab9b51fd5f2e975e39e192e07f24a7e9110
Nikhil <nikhilthakre459@gmail.com>
2026-09-11 23:33:18 +0530

    docs: agent state

--- app/agents/state.py
+++ app/agents/state.py
@@ -2,6 +2,15 @@ from typing import TypedDict, List ,Annotated
 import operator
 
 class AgentState(TypedDict):
+    """
+    This is the state of the agent.
+    - messages: The messages of the agent.
+    - current_query: The current query of the agent.
+    - documents: The documents of the agent.
+    - plan: The plan of the agent.
+    - status: The status of the agent.
+    - final_answer: The final answer of the agent.
+    """
     messages: Annotated[List[dict], operator.add]
     current_query:str
     documents:List[str]
